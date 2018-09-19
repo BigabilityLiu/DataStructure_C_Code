8cc0670e435ce314e237089ef57615b5c62a1dd8
刘旭东 <1130471993@qq.com>
2018-09-19 22:49:14 +0800

    Initial Commit

---
+++ CTutorial/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CTutorial
+//
+//  Created by 刘大本事 on 2018/9/19.
+//  Copyright © 2018年 刘大本事. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
